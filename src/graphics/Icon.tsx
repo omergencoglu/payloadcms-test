ff2eed0674ff1a66cb345352d17859b158eb67bd
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-10-08 19:04:33 -0400

    Personalize admin panel

---
+++ src/graphics/Icon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div>
+      <img src="/assets/logo.png" alt="Omer Gencoglu Logo" />
+    </div>
+  );
+}
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,22 +1,32 @@
-import { buildConfig } from 'payload/config';
-import path from 'path';
-import Users from './collections/Users';
-import { payloadCloud } from '@payloadcms/plugin-cloud';
+import { buildConfig } from "payload/config";
+import path from "path";
+import Users from "./collections/Users";
+import { payloadCloud } from "@payloadcms/plugin-cloud";
+
+import Logo from "./graphics/Logo";
+import Icon from "./graphics/Icon";
 
 export default buildConfig({
   admin: {
     user: Users.slug,
+    meta: {
+      titleSuffix: "- Backend Test",
+      favicon: "/assets/favicon.ico",
+      ogImage: "/assets/logo.png",
+    },
+    components: {
+      graphics: {
+        Logo,
+        Icon,
+      },
+    },
   },
-  collections: [
-    Users,
-  ],
+  collections: [Users],
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
-  plugins: [
-    payloadCloud()
-  ]
+  plugins: [payloadCloud()],
 });
