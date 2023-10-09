5214938a69024a8dd415fde3cc5eed7688bb1113
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-10-09 13:26:30 -0400

    Update payload version to 2.0

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,32 +1,30 @@
-import { buildConfig } from "payload/config";
-import path from "path";
-import Users from "./collections/Users";
-import { payloadCloud } from "@payloadcms/plugin-cloud";
+import path from 'path'
 
-import Logo from "./graphics/Logo";
-import Icon from "./graphics/Icon";
+import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { mongooseAdapter } from '@payloadcms/db-mongodb' // database-adapter-import
+import { webpackBundler } from '@payloadcms/bundler-webpack' // bundler-import
+import { slateEditor } from '@payloadcms/richtext-slate' // editor-import
+import { buildConfig } from 'payload/config'
+
+import Users from './collections/Users'
 
 export default buildConfig({
   admin: {
     user: Users.slug,
-    meta: {
-      titleSuffix: "- Backend Test",
-      favicon: "/assets/favicon.ico",
-      ogImage: "/assets/logo.png",
-    },
-    components: {
-      graphics: {
-        Logo,
-        Icon,
-      },
-    },
+    bundler: webpackBundler(), // bundler-config
   },
+  editor: slateEditor({}), // editor-config
   collections: [Users],
   typescript: {
-    outputFile: path.resolve(__dirname, "payload-types.ts"),
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
   plugins: [payloadCloud()],
-});
+  // database-adapter-config-start
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
+  }),
+  // database-adapter-config-end
+})
